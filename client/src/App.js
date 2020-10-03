8c260f85aa1ee504f0a5a0fcbe3bd4a1c20794e8
presidento23 <johnkenny6799@gmail.com>
2020-10-03 18:59:07 -0400

    Adding single page funcationality

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React, { Fragment } from "react";
 import "./App.css";
 import PaymentSend from "./components/Paymentsend";
-import Paymentsend from "./components/Paymentsend";
-
+import MoneyAdjuster from "./components/Moneyadjuster";
 function App() {
   return (
     <Fragment>
       <PaymentSend />
+      <MoneyAdjuster />
     </Fragment>
   );
 }
---
+++ client/src/components/Moneyadjuster.js
@@ -0,0 +1,27 @@
+import React, { Fragment, useState } from "react";
+
+const Moneyadjuster = () => {
+  return (
+    <Fragment>
+      <table class="table mt-5 text-center">
+        <thead>
+          <tr>
+            <th>Amount</th>
+            <th>Add</th>
+            <th>Subtract</th>
+            <th>Confirm</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* <tr>
+            <td>John</td>
+            <td>Doe</td>
+            <td>john@example.com</td>
+          </tr> */}
+        </tbody>
+      </table>
+    </Fragment>
+  );
+};
+
+export default Moneyadjuster;
