13a5324393382ff010e006c85c148bee44abeeac
presidento23 <johnkenny6799@gmail.com>
2020-10-03 14:41:58 -0400

    Completed functionality of determing validity of an payment recipent

--- client/src/components/Paymentsend.js
+++ client/src/components/Paymentsend.js
@@ -1,9 +1,10 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 
 const PaymentSend = () => {
   const [email, setEmail] = useState("");
-  const onSubmitForm = async (e) => {
-    e.preventDefault();
+  const [found, setFound] = useState(false);
+
+  const fetchData = async () => {
     try {
       const body = { email };
       const response = await fetch("http://localhost:5000/check", {
@@ -12,15 +13,30 @@ const PaymentSend = () => {
         body: JSON.stringify(body),
       });
 
-      console.log(response);
+      const responseData = await response.json();
+      if (responseData === "Email Found") {
+        setFound(true);
+      } else {
+        setFound(false);
+      }
     } catch (err) {
       console.error(err.message);
     }
   };
 
+  useEffect(() => {
+    fetchData();
+  }, [found]);
+
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    fetchData();
+  };
+
   return (
     <Fragment>
       <h1 className="text-center mt-5">Mailpay</h1>
+      <h6 className="text-center mt-3">Enter an Email to send a payment</h6>
       <form className="d-flex mt-5" onSubmit={onSubmitForm}>
         <input
           type="text"
@@ -30,6 +46,15 @@ const PaymentSend = () => {
         />
         <button className="btn btn-success"> Send</button>
       </form>
+      <p className="text-center mt-2">
+        <small>
+          {email.length > 0
+            ? found
+              ? "Email was found"
+              : "Email was not found"
+            : ""}
+        </small>
+      </p>
     </Fragment>
   );
 };
